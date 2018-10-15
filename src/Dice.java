7e6ea2feb229fc3fff9a22ff5039607a11c8390d
AKIRA <AKIRA@AKIRA-PC.fios-router.home>
2018-10-14 23:54:29 -0400

    Worked on it

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,28 @@
+public class Dice
+{
 
-public class Dice {
-
+	private int rollCount;
+	
+	public Dice()
+	{
+		rollCount = 0;
+	}
+	
+	public int roll()
+	{		
+		rollCount++;
+		
+		return (int)(Math.random()*6+1);
+		
+	}
+	
+	public int numRoll()
+	{
+		return rollCount;
+	}
+	
+	public void reset()
+	{
+		rollCount = 0;
+	}
 }
--- src/DiceRollMain.java
+++ src/DiceRollMain.java
@@ -3,6 +3,7 @@
  * @author Akira Morales
  * 
  */
+import java.util.Scanner;
 public class DiceRollMain {
 
 	public static void main(String[] args)
@@ -10,17 +11,38 @@ public class DiceRollMain {
 
 		Dice die1 = new Dice();
 		Dice die2 = new Dice();
-		int rollVal1;
-		int rollVal2;
+		int counter = 0;
+		Scanner in = new Scanner (System.in);
 		
-		
-		while (die1!=die2)
+		/**while (die1.roll() != die2.roll())
 		{
-			die1.roll();
-			die2.roll();
-			
-		}
+			int sum = die1.roll() + die2.roll();
 
+			System.out.println("Value of die 1: " + die1.roll() + "\tValue of die 2: " + die2.roll() + "\tSum: " + sum );
+			counter++;
+		}
+		System.out.println("Number of rolls to get doubles: " + counter);
+		*/
+		System.out.println("Input target sum of dice: ");
+		int target = in.nextInt();
+		in.nextLine();
+		
+		if (target <= 2 || target >=12)
+		{
+			System.out.println("Error- Input invalid, please enter a value from 2 to 12: ");
+			target = in.nextInt();
+		}
+		else if (target >= 2 || target <=12)
+		{
+			while(target != (die1.roll() + die2.roll()))
+			{
+				int sum = die1.roll() + die2.roll();
+
+				System.out.println("Value of die 1: " + die1.roll() + "\tValue of die 2: " + die2.roll() + "\tSum: " + sum );
+				counter++;
+			}
+			System.out.println("Number of rolls to match target value: " + counter);
+		}
 	}
 
 }
